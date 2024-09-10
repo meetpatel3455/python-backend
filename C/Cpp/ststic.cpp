8ae14fb8f69a2f3bae8efed8f97468767d1cbd01
tanviRadadiya112 <radadiyameet37@gmail.com>
2024-09-10 16:36:06 +0530

    10-09-24

---
+++ C/Cpp/ststic.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class base{
+    public:
+    static int number;
+    static int i;
+    base() {
+        number++;
+        i++;
+    }
+    void display(){
+        cout<<"ans is "<<number<<endl;
+    }
+    static void Display(){
+        cout<<"ans is "<<i<<endl;
+    }
+    
+};
+int base::number=10;
+int base::i=0;
+
+
+
+int main(){
+    base a;
+    a.display();
+    base b;
+    b.display();
+    base c;
+    c.display();
+    base d;
+    d.display();
+    return 0;
+
+}
